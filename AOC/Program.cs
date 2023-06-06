9cf285922ab000d0af94a65674d44669a5f7aa57
pypkaed <pypka.eduardovna@yandex.ru>
2023-06-06 13:13:41 +0300

    day one 2022

---
+++ AOC/Program.cs
@@ -0,0 +1,15 @@
+﻿using AOC.Solutions;
+
+namespace AOC;
+
+public class Program
+{
+    public static void Main()
+    {
+        var filePathDayOne = "/home/pypka/RiderProjects/AdventOfCode/AOC/Input/DayOneInput.txt";
+        
+        var dayOne = new DayOne(filePathDayOne);
+        Console.WriteLine(dayOne.SolvePartOne());
+        Console.WriteLine(dayOne.SolvePartTwo());
+    }
+}
---
+++ AOC/Solutions/DayOne.cs
@@ -0,0 +1,73 @@
+namespace AOC.Solutions;
+
+public class DayOne
+{
+    private string _filePath;
+
+    public DayOne(string filePath)
+    {
+        _filePath = filePath;
+    }
+    public class Elf
+    {
+        public int Calories { get; private set; }
+        public void AddCalories(int calories)
+        {
+            Calories += calories;
+        }
+    }
+    
+    public int SolvePartOne()
+    {
+        var elves = InitializeElfCalories(_filePath);
+
+        return FindElfWithMaxCalories(elves).Calories;
+    }
+
+    public int SolvePartTwo()
+    {
+        var elves = InitializeElfCalories(_filePath);
+        return elves.OrderByDescending(elf => elf.Calories).Take(3).Sum(elf => elf.Calories);
+        
+    }
+
+    private List<Elf> InitializeElfCalories(string filePath)
+    {
+        var elves = new List<Elf>();
+        
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string? line;
+            // idk how to do it better for now :(
+            var currentElf = new Elf();
+            elves.Add(currentElf);
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    currentElf = new Elf();
+                    elves.Add(currentElf);
+                }
+                else
+                {
+                    currentElf.AddCalories(int.Parse(line));
+                }
+            }
+        }
+
+        return elves;
+    }
+
+    private Elf FindElfWithMaxCalories(List<Elf> elves)
+    {
+        Elf richestElf = new Elf();
+        
+        foreach (var elf in elves)
+        {
+            richestElf = (elf.Calories > richestElf.Calories) ? elf : richestElf;
+        }
+
+        return richestElf;
+    }
+}
