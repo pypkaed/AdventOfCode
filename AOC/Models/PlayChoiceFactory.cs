d5411eab9df417b84dc9c2328ac3cc67ef7a6b5e
pypkaed <pypka.eduardovna@yandex.ru>
2023-06-11 13:24:23 +0300

    day two

--- AOC/Models/PlayChoiceFactory.cs
+++ AOC/Models/PlayChoiceFactory.cs
@@ -2,19 +2,22 @@ namespace AOC.Models;
 
 public class PlayChoiceFactory
 {
-    private static readonly Dictionary<string, Func<PlayChoice>> _choiceMapping
+    private static readonly Dictionary<string, Func<PlayChoice>> _choiceMappingPartOne
         = new()
         {
             { "A", () => new Rock() },
             { "X", () => new Rock() },
+            { "Rock", () => new Rock() },
             { "B", () => new Paper() },
             { "Y", () => new Paper() },
+            { "Paper", () => new Paper() },
             { "C", () => new Scissors() },
             { "Z", () => new Scissors() },
+            { "Scissors", () => new Scissors() },
         };
 
-    public static PlayChoice GetInstance(string letter)
+    public static PlayChoice GetInstance(string value)
     {
-        return _choiceMapping[letter]();
+        return _choiceMappingPartOne[value]();
     }
 }
--- AOC/Solutions/DayTwo.cs
+++ AOC/Solutions/DayTwo.cs
@@ -14,6 +14,12 @@ public class DayTwo : IDay
     // maybe use yield for this?? i don't like doing I/O in logic method
     public void Solve()
     {
+        SolvePartOne();
+        SolvePartTwo();
+    }
+
+    public void SolvePartOne()
+    {
         int totalScore = 0;
         
         using (var streamReader = new StreamReader(_filePath))
@@ -25,6 +31,54 @@ public class DayTwo : IDay
                 var choices = line.Split(" ");
                 PlayChoice opponentChoice = PlayChoiceFactory.GetInstance(choices[0]);
                 PlayChoice myChoice = PlayChoiceFactory.GetInstance(choices[1]);
+
+                switch (myChoice.Play(opponentChoice))
+                {
+                    case GameOutcome.Lose:
+                        totalScore += myChoice.Weight + 0;
+                        break;
+                    case GameOutcome.Match:
+                        totalScore += myChoice.Weight + 3;
+                        break;
+                    case GameOutcome.Win:
+                        totalScore += myChoice.Weight + 6;
+                        break;
+                }
+            }
+        }
+        
+        Console.WriteLine(totalScore);
+    }
+
+    public void SolvePartTwo()
+    {
+        int totalScore = 0;
+        
+        using (var streamReader = new StreamReader(_filePath))
+        {
+            string? line;
+
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                var choices = line.Split(" ");
+                PlayChoice opponentChoice = PlayChoiceFactory.GetInstance(choices[0]);
+                PlayChoice myChoice;
+                
+                switch (choices[1])
+                {
+                    case "X":
+                        myChoice = PlayChoiceFactory.GetInstance(opponentChoice.winValues[0]);
+                        break;
+                    case "Y":
+                        myChoice = PlayChoiceFactory.GetInstance(opponentChoice.Value);
+                        break;
+                    case "Z":
+                        myChoice = PlayChoiceFactory.GetInstance(opponentChoice.loseValues[0]);
+                        break;
+                    default:
+                        myChoice = null;
+                        break;
+                }
 
                 switch (myChoice.Play(opponentChoice))
                 {
